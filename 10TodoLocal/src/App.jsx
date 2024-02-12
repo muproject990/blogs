e50a2fdbb25e49fe0af4a659866c6939a00bf5e9
Aavash Paudel <webdeveloper98681@gmail.com>
2024-02-13 04:58:54 +0545

    basic hings done

--- 10TodoLocal/src/App.jsx
+++ 10TodoLocal/src/App.jsx
@@ -1,34 +1,53 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  
+  // ?AddTodos functions
+  const addTodos = (todo) => {
+    setTodos(todo => [{ id: Date.now(), ...todo }, ...prev])//! ??? I am confused A LOT
+  }
+
+
+  // ?  updateTodo functions update Todo
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map(prevTodo => (prevTodo.id === id ? todo : prevTodo)))
+    
+  }
+  
+  // ? Delete Todo
+  const deleteTodo = (id) => {
+    setTodos((prev) =>prev.filter(todo=>(todo.id!=id)
+    ))
+  }
+
+
+  // const toggleComlete
+  const toggleComlete = (id) => {
+    setTodos(prev=>prev.map(prevTodo=>prevTodo.id===id? {...prevTodo,completed:!prevTodo.completed
+    } :prevTodo))
+    
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TodoProvider value={{ todos, addTodo, updataTodo, deleteTodo, toggleTodo }}>
+    //!Destructurin in provider to give value from global const context = useContext(contextValue)
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </TodoProvider>
   )
 }
 
---
+++ 10TodoLocal/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from 'react'
+
+export const TodoContext = createContext({
+  todo: [
+    {
+      id: 1,
+      todo: 'Todo Msg',
+      completed: false
+    }
+  ], //Property
+  addTodo: todo => {}, //Methods
+  updataTodo: (id, todo) => {},
+  deleteTodo: id => {},
+  toggleTodo: id => {}
+}) //Used for small projects for bigger projects we use redux
+
+export const useTodo = () => {
+  return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
