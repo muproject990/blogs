27e8fdc4a43d6cc1576111fbcc295dedd00f39f0
Aavash Paudel <webdeveloper98681@gmail.com>
2024-02-14 18:59:08 +0545

    ..

--- SocialMedia/src/App.jsx
+++ SocialMedia/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-console.log(import.meta.env.VITE_APPWRITE_URL);
+console.log();
   return (
     <>
     
